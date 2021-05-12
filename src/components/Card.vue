1eeeb2d684ebbd1160bbda208529428c091c925d
Dima <dimasilolil@gmail.com>
2021-05-12 12:00:42 +0600

    Working

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    {{ name }}
+  <div @click="$emit('delete')" class="card">
+    <div>{{ message }}</div>
+    <div>{{ hour + " : " + minute }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Card",
-  props: {
-    name: String,
+  props: ["delete", "info"],
+  data() {
+    const props = this.info;
+    const min = props.date.getMinutes();
+    return {
+      message: props.message,
+      hour: props.date.getHours(),
+      minute: min > 9 ? min : "0" + min,
+    };
   },
 };
 </script>
+
 <style scoped>
+.card {
+  text-align: start;
+  padding: 3px;
+  border-radius: 5px;
+  margin: 5px;
+  border: 1px solid black;
+  height: 80px;
+  min-width: 150px;
+}
 </style>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <Card v-for="item in testArray" :key="item.message" name="test" />
+    <div id="cards">
+      <Card
+        v-model="mes"
+        v-for="item in array"
+        v-on:delete="removeCard(item.key)"
+        :key="item.key"
+        :info="item"
+      />
+    </div>
+    <div>
+      <input v-model="mes" class="input" type="text" placeholder="Add task" />
+    </div>
+    <button class="input" v-on:click="addCard">Add task</button>
   </div>
 </template>
 
@@ -12,9 +24,40 @@ export default {
   components: {
     Card,
   },
-  data: () => ({ testArray: [{ message: "1" }, { message: "2" }] }),
+  data() {
+    return { array: [], mes: "" };
+  },
+  methods: {
+    addCard() {
+      this.array = [
+        ...this.array,
+        { key: Symbol(), message: this.mes, date: new Date() },
+      ];
+      this.mes = "";
+    },
+    removeCard(key) {
+      this.array = this.array.filter((i) => key !== i.key);
+    },
+  },
 };
 </script>
 
 <style scoped>
+#cards {
+  margin: 15px;
+  overflow-y: auto;
+  height: 300px;
+  border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.input {
+  border: 1px solid black;
+  border-radius: 5px;
+  height: 25px;
+  width: 200px;
+  margin: 5px;
+  background: white;
+}
 </style>
